c3fa6d1c4dc1d49d3854487ecdecf49b6cc199bf
Abdula <you@example.com>
2024-07-10 21:50:39 +0530

    Added readme file

--- calculator.js
+++ calculator.js
@@ -1,10 +1,12 @@
 let calculation = localStorage.getItem('calculation') || '';
 document.querySelector('.js-result').innerHTML = '';
 displayResult();
+//function to calculate
 function updateCalculation(value) {
   calculation += value;
   displayResult();
 }
+//function to show the result
 function displayResult(){
   document.querySelector('.js-result').innerHTML = calculation;
 }
